b7c4499a021670d3c752cef609524e771bdb8931
hubmich <m.polaski@bn.org.pl>
2019-10-30 15:06:22 +0100

    Zrobiłem porządki

--- resttutorial/snippets/serializers.py
+++ resttutorial/snippets/serializers.py
@@ -1,28 +1,8 @@
 from rest_framework import serializers
-from snippets.models import Snippet,LANGUAGE_CHOICES,STYLE_CHOICES
+from snippets.models import Snippet
 
 class SnippetSerializer(serializers.ModelSerializer):
- 
- #class SnippetSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     title= serializers.CharField(required=False, allow_blank=True,max_length=100)
-#     code = serializers.CharField(style={'base_tamplate': 'textarea.html'})
-#     lineos = serializers.BooleanField(required=False)
-#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES,default='python')
-#     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-    
-    #def create(self, validated_data):
-       #return Snippet.objects.create(**validated_data)
-    # 
-    # def update(self, instance, validated_data):
-    #     instance.title = validated_data.get('title', instance.title)
-    #     instance.code = validated_data.get('code', instance.code)
-    #     instance.linos = validated_data.get('linos', instance.linos)
-    #     instance.language = validated_data.get('language', instance.language)
-    #     instance.style = validated_data.get('style', instance.style)
-    #     instance.save()
-    #     return instance
-        
+
     class Meta:
         model = Snippet
         fields = ['id','title','code','lineos','language','style']
--- resttutorial/snippets/views.py
+++ resttutorial/snippets/views.py
@@ -13,5 +13,3 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-
